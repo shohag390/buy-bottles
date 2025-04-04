63d6461855b6629bfaf04de7809e5512b27e991e
shohag390 <shohag390@users.noreply.github.com>
2025-04-04 18:29:09 +0600

    Style Add

--- src/Components/Bottles/Bottle.jsx
+++ src/Components/Bottles/Bottle.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const Bottle = (props) => {
-    const { bottle } = props;
-    const { img } = bottle;
+    const { bottle, handelAddToCart } = props;
+    const { img, name, price, category } = bottle;
 
-    console.log(bottle);
 
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className='border-[1px] border-[#510963] rounded-md p-[15px]'>
+            <img className='rounded-sm' src={img} alt="" />
+            <h4 className='font-medium text-[18px] pt-[5px]'>{name}</h4>
+            <div className='flex items-center justify-between pb-[10px]'>
+                <p>{category}</p>
+                <p>${price}</p>
+            </div>
+            <button onClick={() => handelAddToCart(bottle)} className='py-[10px] w-full bg-[#000] text-[#fff] rounded-md active:scale-[.9] duration-500'>
+                Add To Cart
+            </button>
         </div>
     );
 };
--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,15 +1,28 @@
-import { use } from 'react';
+import { use, useState } from 'react';
 import Bottle from './Bottle';
 
 const Bottles = (props) => {
 
     const { bottlesPromise } = props;
-    console.log(bottlesPromise);
     const bottles = use(bottlesPromise);
+    const [cart, setCart] = useState([])
+    console.log(cart);
+
+
+    const handelAddToCart = (bottle) => {
+        const newCart = [...cart, bottle];
+        setCart(newCart)
+    }
 
     return (
-        <div>
-            {bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} />)}
+        <div className='px-[80px] pb-[30px]'>
+            <div className='font-medium text-[20px] text-[#510963] uppercase pb-[20px] pt-[30px] flex justify-between items-center'>
+                <h4 className=''>Bottles Colection</h4>
+                <p>Cart Item: {cart.length}</p>
+            </div>
+            <div className='grid grid-cols-4 gap-[20px]'>
+                {bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handelAddToCart={handelAddToCart} />)}
+            </div>
         </div>
     );
 };
